2f23c0e8edffc02b3d754765b1fbab4913fe644c
big1808 <CGB@USER-20171228RC>
2018-12-20 15:47:30 +0800

    111111111111

--- src/main/java/com/tedu/controller/UserContrller.java
+++ src/main/java/com/tedu/controller/UserContrller.java
@@ -10,6 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserContrller {
 	@RequestMapping("hi")
 	public String hi() {
-		return "hello           dddd   twolegs!";
 	}
 }
